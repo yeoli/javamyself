6cb2a670cfb05472855331c4db4766cf8b0cfd29
yeoli <71303398+yeoli@users.noreply.github.com>
2020-10-06 09:06:21 +0900

    Update P4_1_5.java

--- src/chap4_1/P4_1_5.java
+++ src/chap4_1/P4_1_5.java
@@ -7,7 +7,9 @@ public class P4_1_5 {
 		int a = ((25+5)+(36/4)-72)*5;
 		int b = ((25*5)+(36-4)+71)/4;
 		int c = (128/4)*2;
-		boolean result;
+		boolean result = a>b && b>c;
+		
+		System.out.println(result);
 		
 	}
 
